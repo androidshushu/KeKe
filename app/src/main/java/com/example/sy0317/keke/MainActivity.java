602322e644b22ee4dd0577c4f4e4f313825f6b7f
sy0317 <719711096@qq.com>
2018-03-08 20:41:47 +0800

    新增app闪屏页,套用第三方框架。

--- app/src/main/java/com/example/sy0317/keke/MainActivity.java
+++ app/src/main/java/com/example/sy0317/keke/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.sy0317.keke;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import za.co.riggaroo.materialhelptutorial.TutorialItem;
+import za.co.riggaroo.materialhelptutorial.tutorial.MaterialTutorialActivity;
 
+public class MainActivity extends Activity {
+    private static final int REQUEST_CODE = 1234;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadTutorial();
+
+    }
+
+
+
+    public void loadTutorial() {
+        Intent mainAct = new Intent(this, MaterialTutorialActivity.class);
+        mainAct.putParcelableArrayListExtra(MaterialTutorialActivity.MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems(this));
+        startActivityForResult(mainAct, REQUEST_CODE);
+
+    }
+
+    private ArrayList<TutorialItem> getTutorialItems(Context context) {
+        TutorialItem tutorialItem1 = new TutorialItem(
+                R.string.slide_1_african_story_books,
+                R.string.slide_1_african_story_books,
+                R.color.slide_1, R.drawable.tut_page_1_front,
+                R.drawable.tut_page_1_background);
+
+        TutorialItem tutorialItem2 = new TutorialItem(
+                R.string.slide_2_volunteer_professionals,
+                R.string.slide_2_volunteer_professionals_subtitle,
+                R.color.slide_2,
+                R.drawable.tut_page_2_front,
+                R.drawable.tut_page_2_background);
+
+        TutorialItem tutorialItem3 = new TutorialItem(context.getString(
+                R.string.slide_3_download_and_go), null,
+                R.color.slide_3,
+                R.drawable.tut_page_3_foreground);
+
+        TutorialItem tutorialItem4 = new TutorialItem(
+                R.string.slide_4_different_languages,
+                R.string.slide_4_different_languages_subtitle,
+                R.color.slide_4,
+                R.drawable.tut_page_4_foreground,
+                R.drawable.tut_page_4_background);
+
+        ArrayList<TutorialItem> tutorialItems = new ArrayList<>();
+        tutorialItems.add(tutorialItem1);
+        tutorialItems.add(tutorialItem2);
+        tutorialItems.add(tutorialItem3);
+        tutorialItems.add(tutorialItem4);
+
+        return tutorialItems;
     }
 }
